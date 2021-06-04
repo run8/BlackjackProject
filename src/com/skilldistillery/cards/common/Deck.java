f5aaf8fabac9e1bb786d2a00845a90c94f86a9f2
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-06-04 14:19:20 -0700

    Project setup

---
+++ src/com/skilldistillery/cards/common/Deck.java
@@ -0,0 +1,35 @@
+package com.skilldistillery.cards.common;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+	private List<Card> cards;
+
+	public Deck() {
+		cards = new ArrayList<>(52);
+
+		for (Suit s : Suit.values()) {
+			for (Rank r : Rank.values()) {
+				Card c = new Card(s, r);
+				cards.add(c);
+			}
+		}
+	}
+
+	public int checkDeckSize() {
+		if (cards == null) {
+			return -1;
+		}
+		return cards.size();
+	}
+
+	public Card dealCard() {
+		return cards.remove(0);
+	}
+
+	public void shuffle() {
+		Collections.shuffle(cards);
+	}
+}
